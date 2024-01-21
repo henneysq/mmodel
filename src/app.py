331e19f9c27b00c586dae593866fddff2318f2c6
Mark Henney <mark@henney.dk>
2024-01-21 15:22:02 +0100

    number input

--- src/app.py
+++ src/app.py
@@ -1,8 +1,5 @@
-from dash import Dash, html, dcc, callback, Output, Input
-import plotly.express as px
-import pandas as pd
 
-df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder_unfiltered.csv')
+from dash import Dash, dcc, html, Input, Output, callback
 
 app = Dash(__name__)
 
@@ -11,22 +8,36 @@ server = app.server
 
 hidden_div = html.Div(id="hidden-div", style={"display": "none"})
 
-app.layout = html.Div([
-    html.H1(children='Title of Dash App', style={'textAlign':'center'}),
-    dcc.Dropdown(df.country.unique(), 'Canada', id='dropdown-selection'),
-    dcc.Graph(id='graph-content')
-])
+app.layout = html.Div(
+    [
+        dcc.Input(id="dfalse", type="number", placeholder="Debounce False"),
+        dcc.Input(
+            id="dtrue", type="number",
+            debounce=True, placeholder="Debounce True",
+        ),
+        dcc.Input(
+            id="input_range_2", type="number", placeholder="input with range",
+            min=10, max=100, step=3,
+        ),
+        html.Hr(),
+        html.Div(id="number-out"),
+    ]
+)
+
 
 @callback(
-    Output('graph-content', 'figure'),
-    Input('dropdown-selection', 'value')
+    Output("number-out", "children"),
+    Input("dfalse", "value"),
+    Input("dtrue", "value"),
+    Input("input_range_2", "value"),
 )
-def update_graph(value):
-    dff = df[df.country==value]
-    return px.line(dff, x='year', y='pop')
+def number_render(fval, tval, rangeval):
+    return "dfalse: {}, dtrue: {}, range: {}".format(fval, tval, rangeval)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
+
 
 
 # '''
